672463aaa3b5634d0a87f892dcb422819c852675
mac <mac@macdeMacBook-Pro-4.local>
2019-05-15 22:38:17 +0800

    misc: 修复对象重名属性问题

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import Icon from './components/icon';
 import Input from './components/input';
 import Radio from './components/radio';
 import Checkbox from './components/checkbox';
-import Switch from './components/switch';
+// import Switch from './components/switch';
 import Search from './components/search';
 import Checker from './components/checker';
 import Range from './components/range';
 import Upload from './components/upload';
 import ProgressBar from './components/ProgressBar';
 import Countdown from './components/countdown';
-import Dialog from './services/dialog/index.vue';
+import DialogComponent from './services/dialog/index.vue';
 //service
 import toast from './services/toast/index.js';
 import dialog from './services/dialog/index.js';
@@ -26,14 +26,14 @@ function install(){
     Vue.component('vu-input', Input);
     Vue.component('vu-radio', Radio);
     Vue.component('vu-checkbox', Checkbox);
-    Vue.component('vu-switch', Switch);
+    // Vue.component('vu-switch', Switch);
     Vue.component('vu-search', Search);
     Vue.component('vu-checker', Checker);
     Vue.component('vu-range', Range);
     Vue.component('vu-upload', Upload);
     Vue.component('vu-progressbar', ProgressBar);
     Vue.component('vu-countdown', Countdown);
-    Vue.component('vu-dialog', Dialog);
+    Vue.component('vu-dialog', DialogComponent);
     //Services
     Vue.prototype.$toast = window.$toast = toast;
     Vue.prototype.$dialog = window.$dialog = dialog;
@@ -47,7 +47,7 @@ export default {
     Input,
     Radio,
     Checkbox,
-    Switch,
+    // Switch,
     Search,
     Checker,
     Range,
